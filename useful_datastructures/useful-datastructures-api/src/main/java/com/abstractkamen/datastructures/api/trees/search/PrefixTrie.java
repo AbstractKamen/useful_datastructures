5ad5412751e994a5421f5b3c44b9c233b022ebc2
KamenHristov <kamenstudentov@gmail.com>
2023-07-04 18:15:59 +0300

    PrefixTrie typos

--- useful_datastructures/useful-datastructures-api/src/main/java/com/abstractkamen/datastructures/api/trees/search/PrefixTrie.java
+++ useful_datastructures/useful-datastructures-api/src/main/java/com/abstractkamen/datastructures/api/trees/search/PrefixTrie.java
@@ -18,10 +18,10 @@ public interface PrefixTrie {
     Collection<String> startsWith(String prefix, int limit);
 
     /**
-     * Checks if a word exists in this tree.
+     * Checks if a word exists in this trie.
      *
      * @param string to check
-     * @return true if word is in this tree
+     * @return true if word is in this trie
      */
     boolean contains(String string);
 
@@ -34,7 +34,7 @@ public interface PrefixTrie {
     boolean isPrefix(String prefix);
 
     /**
-     * Inserts a word into the tree. Duplicate words will be ignored.
+     * Inserts a word into the trie. Duplicate words will be ignored.
      *
      * @param string word to be inserted
      * @return true if the word was inserted false if it already existed
@@ -42,29 +42,29 @@ public interface PrefixTrie {
     boolean insert(String string);
 
     /**
-     * Deletes a word from the tree. The number of complete words will be decremented by one and the size of the tree will shrink
+     * Deletes a word from the trie. The number of complete words will be decremented by one and the size of the trie will shrink
      * by the number of unique characters and their placements in the deleted word if they aren't used by a different word.
      *
      * @param string to be deleted
-     * @return true if word is successfully deleted false if word was not in the tree
+     * @return true if word is successfully deleted false if word was not in the trie
      */
     boolean delete(String string);
 
     /**
-     * The number of nodes in this tree.
+     * The number of nodes in this trie.
      * @return number of nodes
      */
     int size();
 
     /**
-     * The number of complete words in this tree.
+     * The number of complete words in this trie.
      *
      * @return number of complete words
      */
     int completeWords();
 
     /**
-     * A detailed string representation of the structure of this tree.
+     * A detailed string representation of the structure of this trie.
      *
      * @return detailed string
      */

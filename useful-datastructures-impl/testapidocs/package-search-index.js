5182bcaad54e2580e20b23c80f370eec518b3c15
GitHub Action <github-action@users.noreply.github.com>
2023-07-04 14:57:24 +0000

    Site checkin for project Useful Datastructures

--- useful-datastructures-impl/testapidocs/package-search-index.js
+++ useful-datastructures-impl/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.abstractkamen.datastructures.impl.trees.search"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.abstractkamen.datastructures.impl.trees.search"},{"l":"com.abstractkamen.datastructures.impl.trees.search.demos"}];updateSearchResults();

b73aa757942b25be500fe6fdab5a36ab1857fa73
KamenHristov <kamenstudentov@gmail.com>
2023-09-04 10:37:23 +0300

    fix heap test

--- useful_datastructures/useful-datastructures-impl/src/test/java/com/abstractkamen/datastructures/impl/heaps/AdjustableBinaryHeapCompareTest.java
+++ useful_datastructures/useful-datastructures-impl/src/test/java/com/abstractkamen/datastructures/impl/heaps/AdjustableBinaryHeapCompareTest.java
@@ -11,60 +11,91 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class AdjustableBinaryHeapCompareTest {
-    private static final double[] RANDOM_WEIGHTS = new Random().doubles(100, 0, 1000).toArray();
+    private static final double[] RANDOM_WEIGHTS = new Random().doubles(1000, 0, 1000).toArray();
     private final String name;
     private final int numVertices;
     private final int numEdges;
     private final DijkstraAlgorithm dijkstraAlgorithm;
     private final DijkstraAlgorithm dijkstraAlgorithmCompare;
     private double expected;
+    private final Object initialCapacity;
 
     @Parameterized.Parameters(name = "{index}: name={0}, numVertices={1}, numEdges={2}, dijkstraAlgorithm={3}, " +
-        "dijkstraAlgorithmCompare={4}")
+        "dijkstraAlgorithmCompare={4}, initialCapacity={5}")
     public static Iterable<Object[]> data() {
         final BiConsumer<AdjustableBinaryHeap<Vertex>, Vertex> push = AdjustableBinaryHeap::push;
-        final BiConsumer<AdjustableBinaryHeap<Vertex>, Vertex> heapUse = (a, b) -> {
-            a.decreaseKey(b, b);
-        };
-        final BiConsumer<PriorityQueue<Vertex>, Vertex> queueOffer = (q, v) -> {
+        final BiConsumer<AdjustableBinaryHeap<Vertex>, Vertex> decreaseKey = (a, b) -> a.decreaseKey(b, b);
+        final BiConsumer<PriorityQueue<Vertex>, Vertex> removeOffer = (q, v) -> {
             q.remove(v);
             q.offer(v);
         };
-        final int v = 200_000;
+        final int vertices = 200_000;
+        final int edges = 15;
         return Arrays.asList(new Object[][]
-                                 {
-                                     {"AdjustableBinaryHeap", v, 10, DijkstraAlgorithm.createDefault(AdjustableBinaryHeap::pop,
-                                                                                                     push,
-                                                                                                     heapUse,
-                                                                                                     AdjustableBinaryHeap.createComparable(v),
-                                                                                                     AdjustableBinaryHeap::isEmpty),
-                                         DijkstraAlgorithm.createDefault(PriorityQueue::poll,
-                                                                         queueOffer,
-                                                                         queueOffer,
-                                                                         new PriorityQueue<>(v, Vertex::compareTo),
-                                                                         PriorityQueue::isEmpty)},
-                                     // ****************************************************************
-                                     {"PriorityQueue", v, 10, DijkstraAlgorithm.createDefault(PriorityQueue::poll,
-                                                                                              queueOffer,
-                                                                                              queueOffer,
-                                                                                              new PriorityQueue<>(v, Vertex::compareTo),
-                                                                                              PriorityQueue::isEmpty),
-                                         DijkstraAlgorithm.createDefault(AdjustableBinaryHeap::pop,
-                                                                         push,
-                                                                         heapUse,
-                                                                         AdjustableBinaryHeap.createComparable(v),
-                                                                         AdjustableBinaryHeap::isEmpty)},
-                                 }
+                                 {// @formatter:off
+               {"AdjustableBinaryHeap", vertices, edges,
+                   DijkstraAlgorithm.createDefault(AdjustableBinaryHeap::pop,
+                                                   push,
+                                                   decreaseKey,
+                                                   AdjustableBinaryHeap.createComparable(),
+                                                   AdjustableBinaryHeap::isEmpty),
+                   DijkstraAlgorithm.createDefault(PriorityQueue::poll,
+                                                   removeOffer,
+                                                   removeOffer,
+                                                   new PriorityQueue<>(Vertex::compareTo),
+                                                   PriorityQueue::isEmpty),
+                                                   "default"},
+               // ****************************************************************
+               {"PriorityQueue.remove().offer()", vertices, edges,
+                   DijkstraAlgorithm.createDefault(PriorityQueue::poll,
+                                                   removeOffer,
+                                                   removeOffer,
+                                                   new PriorityQueue<>(Vertex::compareTo),
+                                                   PriorityQueue::isEmpty),
+                   DijkstraAlgorithm.createDefault(AdjustableBinaryHeap::pop,
+                                                   push,
+                                                   decreaseKey,
+                                                   AdjustableBinaryHeap.createComparable(),
+                                                   AdjustableBinaryHeap::isEmpty),
+                                                   "default"},
+               // ****************************************************************
+               {"AdjustableBinaryHeap", vertices, edges,
+                   DijkstraAlgorithm.createDefault(AdjustableBinaryHeap::pop,
+                                                   push,
+                                                   decreaseKey,
+                                                   AdjustableBinaryHeap.createComparable(vertices),
+                                                   AdjustableBinaryHeap::isEmpty),
+                   DijkstraAlgorithm.createDefault(PriorityQueue::poll,
+                                                   removeOffer,
+                                                   removeOffer,
+                                                   new PriorityQueue<>(vertices, Vertex::compareTo),
+                                                   PriorityQueue::isEmpty),
+                                                   vertices},
+               // ****************************************************************
+               {"PriorityQueue.remove().offer()", vertices, edges,
+                   DijkstraAlgorithm.createDefault(PriorityQueue::poll,
+                                                   removeOffer,
+                                                   removeOffer,
+                                                   new PriorityQueue<>(vertices,Vertex::compareTo),
+                                                   PriorityQueue::isEmpty),
+                   DijkstraAlgorithm.createDefault(AdjustableBinaryHeap::pop,
+                                                   push,
+                                                   decreaseKey,
+                                                   AdjustableBinaryHeap.createComparable(vertices),
+                                                   AdjustableBinaryHeap::isEmpty),
+                                                   vertices}
+           }// @formatter:on
         );
     }
 
     public AdjustableBinaryHeapCompareTest(String name, int numVertices, int numEdges, DijkstraAlgorithm dijkstraAlgorithm,
-                                           DijkstraAlgorithm dijkstraAlgorithmCompare) {
+                                           DijkstraAlgorithm dijkstraAlgorithmCompare, Object initialCapacity) {
         this.name = name;
         this.numVertices = numVertices;
         this.numEdges = numEdges;
         this.dijkstraAlgorithm = dijkstraAlgorithm;
         this.dijkstraAlgorithmCompare = dijkstraAlgorithmCompare;
+        this.initialCapacity = initialCapacity;
     }
 
     @Before
@@ -83,7 +114,7 @@ public class AdjustableBinaryHeapCompareTest {
         dijkstraAlgorithm.computePath(graph.get(0));
         final double actual = dijkstraAlgorithmCompare.getShortestPathDistanceTo(graph.get(numVertices - 1));
         final long e = System.currentTimeMillis() - s;
-        System.out.printf("%s:%.3f%n", name, e / 1000f);
+        System.out.printf("%s:%.3f initialCapacity=%s%n", name, e / 1000f, initialCapacity);
         // assert
         assertEquals(expected, actual, 0);
         System.out.printf("expected:%f    actual:%f%n", expected, actual);
@@ -123,24 +154,24 @@ public class AdjustableBinaryHeapCompareTest {
 }
 
 class DijkstraAlgorithm {
-    private final Function<Object, Object> heapPoll;
+    private final Function<Object, Object> poll;
     private final Predicate<Object> isEmpty;
-    private final BiConsumer<Object, Object> heapPush;
-    private final BiConsumer<Object, Object> heapUse;
-    private final Object heap;
+    private final BiConsumer<Object, Object> push;
+    private final BiConsumer<Object, Object> use;
+    private final Object dataStructure;
 
-    public DijkstraAlgorithm(Function<Object, Object> heapPoll, BiConsumer<Object, Object> heapPush,
-                             BiConsumer<Object, Object> heapUse, Object heap, Predicate<Object> isEmpty) {
-        this.heapPoll = heapPoll;
-        this.heapPush = heapPush;
-        this.heapUse = heapUse;
-        this.heap = heap;
+    public DijkstraAlgorithm(Function<Object, Object> poll, BiConsumer<Object, Object> push,
+                             BiConsumer<Object, Object> use, Object dataStructure, Predicate<Object> isEmpty) {
+        this.poll = poll;
+        this.push = push;
+        this.use = use;
+        this.dataStructure = dataStructure;
         this.isEmpty = isEmpty;
     }
 
-    public static <H, V> DijkstraAlgorithm createDefault(Function<H, Object> heapPoll, BiConsumer<H, V> heapPush,
-                                                         BiConsumer<H, V> heapUse, H heap, Predicate<H> isEmpty) {
-        return new DijkstraAlgorithm(convert(heapPoll), convert(heapPush), convert(heapUse), heap, convert(isEmpty));
+    public static <H, V> DijkstraAlgorithm createDefault(Function<H, Object> poll, BiConsumer<H, V> push,
+                                                         BiConsumer<H, V> use, H dataStructure, Predicate<H> isEmpty) {
+        return new DijkstraAlgorithm(convert(poll), convert(push), convert(use), dataStructure, convert(isEmpty));
     }
 
     @SuppressWarnings("unchecked")
@@ -149,28 +180,28 @@ class DijkstraAlgorithm {
     }
 
     @SuppressWarnings("unchecked")
-    private static <H> Function<Object, Object> convert(Function<H, Object> c) {
-        return a -> c.apply((H) a);
+    private static <H> Function<Object, Object> convert(Function<H, Object> op) {
+        return a -> op.apply((H) a);
     }
 
     @SuppressWarnings("unchecked")
-    private static <H> Predicate<Object> convert(Predicate<H> c) {
-        return a -> c.test((H) a);
+    private static <H> Predicate<Object> convert(Predicate<H> op) {
+        return a -> op.test((H) a);
     }
 
     public void computePath(Vertex source) {
         // heap
-        heapPush.accept(heap, source);
+        push.accept(dataStructure, source);
         source.setDistance(0);
-        while (!isEmpty.test(heap)) {
-            final Vertex actualVertex = (Vertex) heapPoll.apply(heap);
+        while (!isEmpty.test(dataStructure)) {
+            final Vertex actualVertex = (Vertex) poll.apply(dataStructure);
             for (Edge edge : actualVertex.getAdjacencyList()) {
                 final Vertex v = edge.getTargetVertex();
                 final double d = actualVertex.getDistance() + edge.getWeight();
                 if (d < v.getDistance()) {
                     v.setDistance(d);
                     v.setPredecessor(actualVertex);
-                    heapUse.accept(heap, v);
+                    use.accept(dataStructure, v);
                 }
             }
         }
